a74b47f9e2b7105e11c6d2b663b13ef9ca748f77
YuujiMatuoka <yuuji_matuoka@hotmail.com>
2024-11-21 20:29:17 -0300

    push

--- Assets/Assets/Scripts/Mob/Soldier.cs
+++ Assets/Assets/Scripts/Mob/Soldier.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class Soldier : MonoBehaviour
 {
     public int speed;
     public GameObject target;
     public float direction;
+
+    private PhotonView PhotonView;
+
+    public void Start()
+    {
+        PhotonView = GetComponent<PhotonView>();
+    }
     void Update()
+    {
+        transform.position += new Vector3(-speed*Time.deltaTime,0f,0f); 
+    }
+
+    public void Move()
     {
         direction = Vector3.Distance(transform.position, target.transform.position);
 
         transform.position += transform.position * direction * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CastleDoor"))
+        {
+            Destroy(this);
+        }
     }
 }
