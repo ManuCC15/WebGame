c4bc357605e4425af8a9aeaf76b5658d7d4713d3
YuujiMatuoka <yuuji_matuoka@hotmail.com>
2024-12-12 19:15:56 -0300

    fix popup

--- Assets/Assets/Scripts/Object/PopUp.cs
+++ Assets/Assets/Scripts/Object/PopUp.cs
@@ -1,8 +1,9 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PopUp : MonoBehaviour
+public class PopUp : MonoBehaviourPunCallbacks
 {
     public GameObject PopUpPrefab;
     void Start()
